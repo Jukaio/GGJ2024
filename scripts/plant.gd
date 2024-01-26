496ce8c6c2e8af5341c4a7e888a9e224ea051390
MickeTHS <micke@thinkinghat.se>
2024-01-26 20:53:11 +0100

    more methods

--- scripts/plant.gd
+++ scripts/plant.gd
@@ -1,6 +1,9 @@
 class_name Plant extends Sprite2D
 
 @export var GrowthState : int
+@export var TimeForStateIncrease : float
+
+var timer = 0.0
 
 func set_growth_state(state: int):
 	self.frame = state
@@ -12,13 +15,25 @@ func inc_growth_state():
 		
 	set_growth_state(GrowthState+1)
 
+func pick():
+	queue_free()
+
+
+func get_growth_state():
+	return GrowthState
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	
 	set_growth_state(GrowthState)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	set_growth_state(GrowthState)
+	
+	timer += delta
+	
+	if timer > TimeForStateIncrease:
+		inc_growth_state()
+		timer = 0
 	
