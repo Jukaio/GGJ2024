60544c99fa062d4df53fa20c2794beccee8e586b
MickeTHS <micke@thinkinghat.se>
2024-01-26 20:35:49 +0100

    mushroom grid

--- scripts/grid.gd
+++ scripts/grid.gd
@@ -1,11 +1,47 @@
-extends Node
+class_name CropsGrid extends Node
 
+@export var NumCols : int = 16
+@export var NumRows : int = 16
 
-# Called when the node enters the scene tree for the first time.
+@export var WidthPixels : int = 16
+@export var HeightPixels : int = 16
+
+@onready var MushroomPrefab1 = preload("res://prefabs/mushroom_sprite.tscn") as PackedScene
+
+var rng = RandomNumberGenerator.new()
+
+var growth_timer = 0.0
+
+var plant_data = []
+
+func get_plant_at(row: int, col: int):
+	
+	return plant_data[row][col]
+	
+	
+
 func _ready():
-	pass # Replace with function body.
+	
+	for row in range(NumCols):
+		plant_data.append([])
+		
+		for col in range(NumRows):
+			print("spawned" + str(col) + " " + str(row))
+			var newobj = MushroomPrefab1.instantiate() as Plant
+			add_child(newobj)
+			
+			newobj.set_growth_state(0)
+			newobj.global_position = Vector2(WidthPixels * col, HeightPixels * row)
+			plant_data[row].append(newobj)
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	
+	growth_timer += delta
+	
+	if growth_timer > 0.1:
+		growth_timer = 0.0
+		var plant = get_plant_at(rng.randi_range(0, NumRows-1), rng.randi_range(0, NumCols-1)) as Plant
+		
+		plant.inc_growth_state()
+	
