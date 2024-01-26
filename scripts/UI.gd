6532c1ade92ac0fb403dd7405b44397e825c06eb
MickeTHS <micke@thinkinghat.se>
2024-01-26 23:46:59 +0100

    Main Menu UI

---
+++ scripts/UI.gd
@@ -0,0 +1,15 @@
+class_name UI extends CanvasLayer
+
+@onready var MainMenuUI : Control = $MainMenu
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	
+	if Input.is_action_just_pressed("Start"):
+		MainMenuUI.visible = false
