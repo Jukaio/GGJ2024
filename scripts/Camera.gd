4455a1a05a89c81a3da2adc1a0015fb58849f1f9
David <david@David-Naued.local>
2024-01-28 11:33:44 +0100

    add cshake

---
+++ scripts/Camera.gd
@@ -0,0 +1,23 @@
+extends Camera2D
+
+@export var random_strength: float = 5.0
+@export var decay: float = 30.0
+
+var rng = RandomNumberGenerator.new()
+
+var shake_strength:float = 0.0
+
+func apply_shake(strength: float):
+	shake_strength = strength
+	
+# TODO: Add in some sort of rotation reset.
+func _process(delta):  
+	if shake_strength > 0.1:
+		shake_strength = lerpf(shake_strength, 0, decay * delta)
+		
+		offset = random_offset()
+	else:
+		offset = Vector2.ZERO
+
+func random_offset() -> Vector2:
+	return Vector2(rng.randf_range(-shake_strength, shake_strength),rng.randf_range(-shake_strength, shake_strength))
