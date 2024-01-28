91172758badec389b8c4dcda862492b0845c5b67
MickeTHS <micke@thinkinghat.se>
2024-01-28 16:55:16 +0100

    removed unused assets + main menu fallback

--- scripts/main_menu.gd
+++ scripts/main_menu.gd
@@ -1,13 +1,16 @@
 extends Node
 
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	
 	if Input.is_action_just_pressed("Start"):
+		get_tree().change_scene_to_file("res://scenes/entry_point.tscn")
+		
+func _unhandled_key_input(event):
+	# fallback to make sure we can proceed on any input
+	if event.is_pressed():
 		get_tree().change_scene_to_file("res://scenes/entry_point.tscn")
